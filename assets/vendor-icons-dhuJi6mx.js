dc5c8ffd9c2a3a8f98f86f6624b5e7319c10dcce
pankajkumargnc <pankajkumargnc@gmail.com>
2026-03-15 22:43:40 +0530

    Updates

--- assets/vendor-icons-dhuJi6mx.js
+++ assets/vendor-icons-dhuJi6mx.js
@@ -1,4 +1,4 @@
-import{r as n}from"./vendor-Bb75PWww.js";/**
+import{r as n}from"./vendor-C0I1xKGY.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
